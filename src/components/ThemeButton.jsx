8c92c043e24a0233ca7378c88db71e92b7e99738
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-15 00:23:12 +0530

    Update missed Theme configurations

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,24 +1,24 @@
-import { MdDarkMode, MdSunny } from "react-icons/md";
-import { useTheme } from "../hooks/ThemeProvider.jsx";
+import {MdDarkMode, MdSunny} from "react-icons/md";
+import {useTheme} from "../hooks/ThemeProvider.jsx";
 
 const ThemeButton = () => {
-    const { currentTheme, toggleTheme } = useTheme();
+  const {currentTheme, toggleTheme} = useTheme();
 
-    const themeIcons = {
-        dark: <MdDarkMode className="h-5 w-5 text-white" />,
-        light: <MdSunny className="h-5 w-5 text-black" />,
-    };
+  const themeIcons = {
+    dark: <MdDarkMode className="h-5 w-5 text-white"/>,
+    light: <MdSunny className="h-5 w-5 text-gray-900"/>,
+  };
 
-    return (
-        <>
-            <button
-                className="bg-grey-900 h-fit w-fit rounded-md dark:bg-black"
-                onClick={toggleTheme}
-            >
-                {currentTheme === "light" ? themeIcons.light : themeIcons.dark}
-            </button>
-        </>
-    );
+  return (
+    <>
+      <button
+        className="h-fit w-fit rounded-md"
+        onClick={toggleTheme}
+      >
+        {currentTheme === "light" ? themeIcons.light : themeIcons.dark}
+      </button>
+    </>
+  );
 };
 
 export default ThemeButton;
--- src/hooks/ThemeProvider.jsx
+++ src/hooks/ThemeProvider.jsx
@@ -4,51 +4,51 @@ import PropTypes from "prop-types";
 const ThemeContext = createContext(undefined);
 
 export const useTheme = () => {
-    return useContext(ThemeContext);
+  return useContext(ThemeContext);
 };
 
 export const ThemeProvider = ({children}) => {
-    const [currentTheme, setCurrentTheme] = useState(
-        localStorage.theme === "dark" ? "dark" : "light",
-    );
+  const [currentTheme, setCurrentTheme] = useState(
+    localStorage.theme === "dark" ? "dark" : "light",
+  );
 
-    useEffect(() => {
-        const ThemeSwitcher = () => {
-            if (
-                localStorage.theme === "dark" ||
-                (!("theme" in localStorage) &&
-                    window.matchMedia("(prefers-color-scheme: dark)").matches)
-            ) {
-                document.documentElement.classList.add("dark");
-                document.documentElement.style.setProperty(
-                    "--bg-color",
-                    "#fff",
-                );
-            } else {
-                document.documentElement.classList.remove("dark");
-                document.documentElement.style.setProperty(
-                    "--bg-color",
-                    "#000",
-                );
-            }
-        };
-
-        ThemeSwitcher();
-    }, [currentTheme]);
-
-    const toggleTheme = () => {
-        const newTheme = currentTheme === "light" ? "dark" : "light";
-        setCurrentTheme(newTheme);
-        localStorage.theme = newTheme;
+  useEffect(() => {
+    const ThemeSwitcher = () => {
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        document.documentElement.classList.add("dark");
+        document.documentElement.style.setProperty(
+          "--bg-color",
+          "#fff",
+        );
+      } else {
+        document.documentElement.classList.remove("dark");
+        document.documentElement.style.setProperty(
+          "--bg-color",
+          "#000",
+        );
+      }
     };
 
-    return (
-        <ThemeContext.Provider value={{currentTheme, toggleTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    );
+    ThemeSwitcher();
+  }, [currentTheme]);
+
+  const toggleTheme = () => {
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    setCurrentTheme(newTheme);
+    localStorage.theme = newTheme;
+  };
+
+  return (
+    <ThemeContext.Provider value={{currentTheme, toggleTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 ThemeProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };

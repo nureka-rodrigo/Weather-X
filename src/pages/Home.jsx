9325b35f42f5c37282087a04eff2643ed765ba12
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-15 15:37:36 +0530

    Display an error message if location access is not granted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import {FaCloud} from "react-icons/fa";
 import {WeatherIcons} from "../data/WeatherIcons.jsx";
 
 const Home = () => {
+  const [locationError, setLocationError] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
@@ -39,10 +40,12 @@ const Home = () => {
           },
           error => {
             console.error('Error getting geolocation:', error);
+            setLocationError(true);
           }
         );
       } else {
         console.error("Geolocation is not supported by this browser.");
+        setLocationError(true);
       }
     };
 
@@ -51,49 +54,60 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-h-screen">
-        <Navbar/>
-        <section>
-          <div className="container px-6 pt-12 md:pt-20 mx-auto text-center flex flex-col justify-center items-center">
-            <div className="flex justify-center mt-10">
-              {weatherData && WeatherIcons[weatherData.weather[0].main]}
+      {locationError ? (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          <h1 className="text-xl font-semibold mb-2">Error: Location access not granted</h1>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          {
+            <div className="flex flex-col justify-center items-center min-h-screen">
+              <Navbar/>
+              <section>
+                <div
+                  className="container px-6 pt-12 md:pt-20 mx-auto text-center flex flex-col justify-center items-center">
+                  <div className="flex justify-center mt-10">
+                    {weatherData && WeatherIcons[weatherData.weather[0].main]}
+                  </div>
+                  <h1
+                    className="text-4xl font-bold text-gray-900 dark:text-white duration-300 ease-linear">{weatherData?.name}</h1>
+                  <p
+                    className="text-lg mt-4 text-gray-500 dark:text-gray-300 duration-300 ease-linear">{weatherData?.weather[0].main}</p>
+                  <p
+                    className="text-lg mt-4 text-gray-500 dark:text-gray-300 duration-300 ease-linear">{weatherData?.main.temp}&deg;C</p>
+                </div>
+                <div
+                  className="max-w-7xl mx-auto font-[sans-serif] mt-0 md:mt-20 mb-12 lg:mb-0 text-gray-950 dark:text-white">
+                  <div
+                    className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-x-36 gap-y-20 mt-16 duration-300 ease-linear">
+                    <div className="text-center">
+                      <WiHumidity className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
+                      <h3 className="text-xl font-semibold mb-2">Humidity</h3>
+                      <p className="text-gray-500 text-sm">{weatherData?.main.humidity}%</p>
+                    </div>
+                    <div className="text-center">
+                      <GiWeightScale className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
+                      <h3 className="text-xl font-semibold mb-2">Pressure</h3>
+                      <p className="text-gray-500 text-sm">{weatherData?.main.pressure} hPa</p>
+                    </div>
+                    <div className="text-center">
+                      <FiWind className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
+                      <h3 className="text-xl font-semibold mb-2">Wind</h3>
+                      <p className="text-gray-500 text-sm">{weatherData?.wind.speed} km/h</p>
+                    </div>
+                    <div className="text-center">
+                      <FaCloud className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
+                      <h3 className="text-xl font-semibold mb-2">Cloudiness</h3>
+                      <p className="text-gray-500 text-sm">{weatherData?.clouds.all}%</p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <Footer className="mt-auto"/>
             </div>
-            <h1
-              className="text-4xl font-bold text-gray-900 dark:text-white duration-300 ease-linear">{weatherData?.name}</h1>
-            <p
-              className="text-lg mt-4 text-gray-500 dark:text-gray-300 duration-300 ease-linear">{weatherData?.weather[0].main}</p>
-            <p
-              className="text-lg mt-4 text-gray-500 dark:text-gray-300 duration-300 ease-linear">{weatherData?.main.temp}&deg;C</p>
-          </div>
-          <div
-            className="max-w-7xl mx-auto font-[sans-serif] mt-0 md:mt-20 mb-12 lg:mb-0 text-gray-950 dark:text-white">
-            <div
-              className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-x-36 gap-y-20 mt-16 duration-300 ease-linear">
-              <div className="text-center">
-                <WiHumidity className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
-                <h3 className="text-xl font-semibold mb-2">Humidity</h3>
-                <p className="text-gray-500 text-sm">{weatherData?.main.humidity}%</p>
-              </div>
-              <div className="text-center">
-                <GiWeightScale className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
-                <h3 className="text-xl font-semibold mb-2">Pressure</h3>
-                <p className="text-gray-500 text-sm">{weatherData?.main.pressure} hPa</p>
-              </div>
-              <div className="text-center">
-                <FiWind className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
-                <h3 className="text-xl font-semibold mb-2">Wind</h3>
-                <p className="text-gray-500 text-sm">{weatherData?.wind.speed} km/h</p>
-              </div>
-              <div className="text-center">
-                <FaCloud className="w-12 h-12 mb-4 inline-block bg-blue-100 p-3 rounded-xl text-gray-950"/>
-                <h3 className="text-xl font-semibold mb-2">Cloudiness</h3>
-                <p className="text-gray-500 text-sm">{weatherData?.clouds.all}%</p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <Footer className="mt-auto"/>
-      </div>
+          }
+        </div>
+      )}
     </>
   );
 };

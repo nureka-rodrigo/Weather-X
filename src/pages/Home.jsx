07071436e078eba822e537b0a5159b54a37014b0
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-15 18:06:41 +0530

    Decrease the font size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -162,14 +162,14 @@ const Home = () => {
                           className="relative flex flex-col mt-6 text-gray-700 bg-blue-100 shadow-md bg-clip-border rounded-xl w-28 mr-4">
                           <div className="p-6">
                             <h5
-                              className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                              className="block mb-2 font-sans antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                               {new Date(forecast.dt * 1000).toLocaleTimeString()}
                             </h5>
                             <div className="flex items-center justify-center">
                               {WeatherIconsForecast[forecast.weather[0].main]}
                             </div>
                             <h5
-                              className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                              className="block mb-2 font-sans antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                               {Math.round(forecast.main.temp)}&deg;C
                             </h5>
                           </div>
